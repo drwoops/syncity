99bb456c9481af882e5191491ad4086b5974ef78
drwoops <thedrwoops@gmail.com>
2021-12-14 06:42:35 +0100

    use type of statistic for conditionals. skip UNKNOWN entity type

--- src/main/java/me/drwoops/syncity/plugins/StatisticsPlugin.java
+++ src/main/java/me/drwoops/syncity/plugins/StatisticsPlugin.java
@@ -35,28 +35,29 @@ public class StatisticsPlugin extends SyncityPlugin {
     public JSONObject get(Player player) {
         JSONObject data = new JSONObject();
         for (Statistic stat: Statistic.values()) {
-            if (stat.isSubstatistic()) {
+            Statistic.Type styp = stat.getType();
+            if (styp == Statistic.Type.UNTYPED) {
+                // simple statistic
+                int n = player.getStatistic(stat);
+                data.put(stat.getKey().toString(), n);
+            } else {
                 JSONObject subdata = new JSONObject();
-                if (stat.isBlock()) {
-                    // block
+                if (styp == Statistic.Type.ENTITY) {
+                    // entity
+                    for (EntityType typ : EntityType.values()) {
+                        if (typ == EntityType.UNKNOWN) continue;
+                        int n = player.getStatistic(stat, typ);
+                        if (n > 0) subdata.put(typ.getKey().toString(), n);
+                    }
+                } else {
+                    // block or item
                     for (Material mat : Material.values()) {
                         int n = player.getStatistic(stat, mat);
                         if (n > 0) subdata.put(mat.getKey().toString(), n);
                     }
-                    if (subdata.length() != 0)
-                        data.put(stat.getKey().toString(), subdata);
-                } else {
-                    // entity
-                    for (EntityType typ : EntityType.values()) {
-                        int n = player.getStatistic(stat, typ);
-                        if (n > 0) subdata.put(typ.getKey().toString(), n);
-                    }
                 }
-                data.put(stat.getKey().toString(), subdata);
-            } else {
-                // simple statistic
-                int n = player.getStatistic(stat);
-                data.put(stat.getKey().toString(), n);
+                if (subdata.length() != 0)
+                    data.put(stat.getKey().toString(), subdata);
             }
         }
         return data;
@@ -68,26 +69,28 @@ public class StatisticsPlugin extends SyncityPlugin {
         for (Statistic stat: Statistic.values()) {
             String name = stat.getKey().toString();
             if (!data.has(name)) continue;
-            if (stat.isSubstatistic()) {
+            Statistic.Type styp = stat.getType();
+            if (styp == Statistic.Type.UNTYPED) {
+                // simple statistic
+                player.setStatistic(stat, data.getInt(name));
+            } else {
                 JSONObject subdata = data.getJSONObject(name);
-                if (stat.isBlock()) {
-                    // block
+                if (styp == Statistic.Type.ENTITY) {
+                    // entity
+                    for (EntityType typ: EntityType.values()) {
+                        if (typ == EntityType.UNKNOWN) continue;
+                        String subname = typ.getKey().toString();
+                        if (subdata.has(subname))
+                            player.setStatistic(stat, typ, subdata.getInt(subname));
+                    }
+                } else {
+                    // block or item
                     for (Material mat: Material.values()) {
                         String subname = mat.getKey().toString();
                         if (subdata.has(subname))
                             player.setStatistic(stat, mat, subdata.getInt(subname));
                     }
-                } else {
-                    // entity
-                    for (EntityType typ: EntityType.values()) {
-                        String subname = typ.getKey().toString();
-                        if (subdata.has(subname))
-                            player.setStatistic(stat, typ, subdata.getInt(subname));
-                    }
                 }
-            } else {
-                // simple statistic
-                player.setStatistic(stat, data.getInt(name));
             }
         }
     }
