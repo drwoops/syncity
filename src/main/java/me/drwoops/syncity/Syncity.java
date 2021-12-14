8c992315fba755a90d119f8cb5c60dd41e8abf76
drwoops <thedrwoops@gmail.com>
2021-12-14 05:20:47 +0100

    place syncity under the lgplv3 license

--- src/main/java/me/drwoops/syncity/Syncity.java
+++ src/main/java/me/drwoops/syncity/Syncity.java
@@ -1,4 +1,22 @@
-// copyright (c) 2012 by drwoops <thedrwoops@gmail.com>
+/*
+ Copyright (c) 2021 by drwoops <thedrwoops@gmail.com>
+
+ This file is part of syncity.
+
+ Syncity is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Syncity is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package me.drwoops.syncity;
 
 import me.drwoops.syncity.plugins.*;
